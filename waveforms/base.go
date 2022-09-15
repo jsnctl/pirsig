0488d16cab41626291d2cea2c7475b2879091ca6
Jason Costello <jason.costello@synapt.ec>
2022-09-15 22:08:12 +0100

    Adding Square

--- waveforms/base.go
+++ waveforms/base.go
@@ -3,9 +3,17 @@ package waveforms
 import "math"
 
 var Lookup = map[string]func(float64, float64) float64{
-	"sine": Sine,
+	"sine":   Sine,
+	"square": Square,
 }
 
 func Sine(angle float64, frequency float64) float64 {
 	return math.Sin(angle * frequency)
+}
+
+func Square(angle float64, frequency float64) float64 {
+	if Sine(angle, frequency) >= 0 {
+		return 1.0
+	}
+	return -1.0
 }
